6aea2c93b938bdcb2464c26e4eb7fa4cb8c323a4
Alexandra.Yakuba <alexandra.yakuba@inbox.ru>
2022-07-14 14:00:37 +0300

    refactoring tests

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,8 @@
 import { capitalize } from '../src/capitalize.js';
+import { strict as assert } from 'assert';
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неправильно');
-}
 
-if (capitalize('') !== '') {
-    throw new Error('Функция работает неправильно');
-}
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
 
 console.log('Все тесты пройдены!');
